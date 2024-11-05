934f88f00e987149c0bff801bbe311de2d4ded8b
theBappy <abappy575@gmail.com>
2024-11-05 18:53:33 +0600

    done

---
+++ starter/errors/index.js
@@ -0,0 +1,10 @@
+const CustomAPIError = require('./custom-error')
+const BadRequestError = require('./bad-requests')
+const UnauthenticatedError = require('./unauthenticated')
+
+
+module.exports = {
+    CustomAPIError, 
+    BadRequestError,
+    UnauthenticatedError,
+}
